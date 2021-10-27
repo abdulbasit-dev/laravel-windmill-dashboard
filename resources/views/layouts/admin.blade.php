273f2eb658679720fa33f9e1cc9efaf597d3d57f
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-10-27 09:48:37 +0300

    first commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Windmill Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{asset('assets/css/tailwind.output.css')}}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/Chart.min.css') }}"/>
+</head>
+<body>
+<div
+        class="flex h-screen bg-gray-50 dark:bg-gray-900"
+        :class="{ 'overflow-hidden': isSideMenuOpen }"
+>
+    <!-- Desktop sidebar -->
+    @include('includes.desktop-sidebar')
+
+    <!-- Mobile sidebar -->
+    @include('includes.mobile-sidebar')
+
+    <div class="flex flex-col flex-1 w-full">
+        @include('includes.header')
+        <main class="h-full overflow-y-auto">
+            @yield('content')
+        </main>
+    </div>
+</div>
+
+<script src="{{ asset("assets/js/alpine.min.js") }}" defer></script>
+<script src="{{ asset("assets/js/Chart.min.js") }}" defer></script>
+<script src="{{ asset("assets/js/init-alpine.js") }}"></script>
+<script src="{{ asset("assets/js/charts-lines.js") }}" defer></script>
+<script src="{{ asset("assets/js/charts-pie.js") }}" defer></script>
+</body>
+</html>
