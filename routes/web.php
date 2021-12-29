8cb6e726c36d5052425dd7cf962fa4b16fe74660
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-12-29 13:36:38 +0300

    make redircet to dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 
 Route::get('/dashboard', function () {
     return view('admin.index');
