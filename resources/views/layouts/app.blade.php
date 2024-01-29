1c8554af09aba25d067a605c3e86ab509cc51655
abdulbasit-dev <abdullbasit99salah@gmail.com>
2024-01-29 11:20:54 +0300

    Refactor: Add meta tag and tab icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+        {{-- favicon --}}
+        <link rel="icon" sizes="180x180" href="{{ asset('assets/img/laravel.png') }}">
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        {{-- favicon --}}
+        <link rel="icon" sizes="180x180" href="{{ asset('assets/img/laravel.png') }}">
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
